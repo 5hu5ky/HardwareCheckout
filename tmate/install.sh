5hu5ky/HardwareCheckout

commit 86b37f358a1a16f406797d26c59b7c245552b452

trivial: fix whitespace and alignment in CHV text banner

tmate/install.sh
@@ -43,26 +43,26 @@ if [[ "$REINSTALL" -eq 1 ]]; then
 cat <<"EOF" | sudo -u $UNAME tee -a /home/$UNAME/.bashrc > /dev/null
 
 
-echo "  
+echo "
 
 
-        
+
                                       _.-=\"\"_-         _
                                  _.-=\"  \"_-           | ||\"\"\"\"\"\"\"-\"--_______     __..
-                     ___.===\"\"\"\"-.______-,,,,,,,,,,,,,-\\''----\" \"\"\"\"\"      \"\"\"\"\" \"_ 
+                     ___.===\"\"\"\"-.______-,,,,,,,,,,,,,-\\''----\" \"\"\"\"\"      \"\"\"\"\" \"_
               __.--\"\"     __        ,'                   o \\           __        [_|
          __-\"\"=======.--\"\"  \"\"--.=================================.--\"\"  \"\"--.=======:
         ]       [w] : /        \ : |== Welcome to the ======|    : /        \ :  [w] :
         V___________:|          |: |= Car Hacking Village ==|    :|          |:   _-
          V__________: \        / :_|=======================/_____: \        / :__-
-         -----------'  \"-____-\"  --------------------------------'  \"-____-\"
+         -----------'  \"-____-\"  ------------------------------'  \"-____-\"
 
 
 
         Welcome to the Car Hacking Village.  This is SUPER BETA!
         If you need help find us on the discord or slack or by phone at 617-440-8667
 	Please wait while we set things up for you to hack...
-	
+
 	**** PLEASE NOTE - TERMINTATING BASH WILL TERMINATE YOUR SESSION! DON'T CRY LATER!!!! ****
 "
 EOF

commit c5dd91f1d4eb45a89f12587c92c3e40d529277a1

fix wheel alignment I broke in 86b37f358a1a16f406797d26c59b7c245552b452

tmate/install.sh
@@ -68,7 +68,7 @@ echo "
         ]       [w] : /        \ : |== Welcome to the ======|    : /        \ :  [w] :
         V___________:|          |: |= Car Hacking Village ==|    :|          |:   _-
          V__________: \        / :_|=======================/_____: \        / :__-
-         -----------'  \"-____-\"  ------------------------------'  \"-____-\"
+         -----------'  \"-____-\"  --------------------------------'  \"-____-\"
 
 
 